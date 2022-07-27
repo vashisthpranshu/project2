469b0eefde175029de44f4242ae8534634fd4e65
Pranshu <pranshu31399@gmail.com>
2022-07-27 17:07:19 +0530

    updated java file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Integration of Github with Jenkins!!");
+    System.out.println("Integration of Jenkins with Docker!!");
   }
 }
