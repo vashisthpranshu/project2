32447dfb760a70518f8dbb922bbc637fad7aec68
vashisthpranshu <80090197+vashisthpranshu@users.noreply.github.com>
2022-08-03 19:52:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Integration of Github with Jenkins!!");
     System.out.println("Integration of Jenkins with Docker!!");
+    System.out.println("Every push triggers a build!!");
   }
 }
